335674a8d4a263834ddeb3ea4caa6185c8c5b26c
davek44 <drk@calicolabs.com>
2023-04-20 14:04:12 -0700

    Add eval

---
+++ baskerville/bed.py
@@ -0,0 +1,171 @@
+# Copyright 2023 Calico LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========================================================================
+
+import json
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import pysam
+from tqdm import tqdm
+
+from baskerville import dna_io
+
+################################################################################
+# bed.py
+#
+# Methods to work with BED files.
+################################################################################
+
+def make_bed_seqs(bed_file, fasta_file, seq_len, stranded=False):
+  """Return BED regions as sequences and regions as a list of coordinate
+  tuples, extended to a specified length."""
+  """Extract and extend BED sequences to seq_len."""
+  fasta_open = pysam.Fastafile(fasta_file)
+
+  seqs_dna = []
+  seqs_coords = []
+
+  for line in open(bed_file):
+    a = line.split()
+    chrm = a[0]
+    start = int(float(a[1]))
+    end = int(float(a[2]))
+    if len(a) >= 6:
+      strand = a[5]
+    else:
+      strand = '+'
+
+    # determine sequence limits
+    mid = (start + end) // 2
+    seq_start = mid - seq_len//2
+    seq_end = seq_start + seq_len
+
+    # save
+    if stranded:
+      seqs_coords.append((chrm,seq_start,seq_end,strand))
+    else:
+      seqs_coords.append((chrm,seq_start,seq_end))
+
+    # initialize sequence
+    seq_dna = ''
+
+    # add N's for left over reach
+    if seq_start < 0:
+      print('Adding %d Ns to %s:%d-%s' % \
+          (-seq_start,chrm,start,end), file=sys.stderr)
+      seq_dna = 'N'*(-seq_start)
+      seq_start = 0
+
+    # get dna
+    seq_dna += fasta_open.fetch(chrm, seq_start, seq_end).upper()
+
+    # add N's for right over reach
+    if len(seq_dna) < seq_len:
+      print('Adding %d Ns to %s:%d-%s' % \
+          (seq_len-len(seq_dna),chrm,start,end), file=sys.stderr)
+      seq_dna += 'N'*(seq_len-len(seq_dna))
+
+    # reverse complement
+    if stranded and strand == '-':
+      seq_dna = dna_io.dna_rc(seq_dna)
+
+    # append
+    seqs_dna.append(seq_dna)
+
+  fasta_open.close()
+
+  return seqs_dna, seqs_coords
+
+
+def read_bed_coords(bed_file, seq_len):
+  """Return BED regions as a list of coordinate
+  tuples, extended to a specified length."""
+  seqs_coords = []
+
+  for line in open(bed_file):
+    a = line.split()
+    chrm = a[0]
+    start = int(float(a[1]))
+    end = int(float(a[2]))
+
+    # determine sequence limits
+    mid = (start + end) // 2
+    seq_start = mid - seq_len//2
+    seq_end = seq_start + seq_len
+
+    # save
+    seqs_coords.append((chrm,seq_start,seq_end))
+
+  return seqs_coords
+
+
+def write_bedgraph(preds, targets, data_dir: str, out_dir: str, split_label: str, bedgraph_indexes=None):
+  """Write BEDgraph files for predictions and targets from a dataset..
+  
+  Args:
+    preds (np.array): Predictions.
+    targets (np.array): Targets.
+    data_dir (str): Data directory, for identifying sequences and statistics.
+    out_dir (str): Output directory.
+    split_label (str): Split label.
+    bedgraph_indexes (list): List of target indexes to write.
+  """
+  # get shapes
+  num_seqs, target_length, num_targets = targets.shape
+
+  # set bedgraph indexes
+  if bedgraph_indexes is None:
+    bedgraph_indexes = np.arange(num_targets)
+
+  # read data parameters
+  with open('%s/statistics.json'%data_dir) as data_open:
+    data_stats = json.load(data_open)
+    pool_width = data_stats['pool_width']
+
+  # read sequence positions
+  seqs_df = pd.read_csv('%s/sequences.bed'%data_dir, sep='\t',
+                        names=['chr','start','end','split'])
+  seqs_df = seqs_df[seqs_df.split == split_label]
+  assert(seqs_df.shape[0] == num_seqs)
+
+  # initialize output directory
+  os.makedirs(out_dir, exist_ok=True)
+
+  print('Writing BEDgraph files')
+  for ti in tqdm(bedgraph_indexes):
+    # slice preds/targets
+    preds_ti = preds[:,:,ti]
+    targets_ti = targets[:,:,ti]
+
+    # initialize raw predictions/targets
+    preds_out = open('%s/preds_t%d.bedgraph' % (out_dir, ti), 'w')
+    targets_out = open('%s/targets_t%d.bedgraph' % (out_dir, ti), 'w')
+
+    # write raw predictions/targets
+    for si, seq in enumerate(seqs_df.itertuples()):
+      # write bin values
+      bin_start = seq.start
+      for bi in range(target_length):
+        bin_end = bin_start + pool_width
+        cols = [seq.chr, str(bin_start), str(bin_end), '%.2f'%preds_ti[si,bi]]
+        print('\t'.join(cols), file=preds_out)
+        cols = [seq.chr, str(bin_start), str(bin_end), '%.2f'%targets_ti[si,bi]]
+        print('\t'.join(cols), file=targets_out)
+        bin_start = bin_end
+
+    preds_out.close()
+    targets_out.close()
---
+++ scripts/hound_eval.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# Copyright 2023 Calico LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========================================================================
+import argparse
+import json
+import os
+
+import h5py
+import numpy as np
+import pandas as pd
+from scipy.stats import spearmanr
+import tensorflow as tf
+from tqdm import tqdm
+
+from baskerville import bed
+from baskerville import dataset
+from baskerville import seqnn
+from baskerville import trainer
+
+"""
+hound_eval.py
+
+Evaluate the accuracy of a trained model on held-out sequences.
+"""
+def main():
+  parser = argparse.ArgumentParser(description='Evaluate a trained model.')
+  parser.add_argument('-b', '--bedgraph_indexes',
+                      help='Comma-separated list of target indexes to write predictions/targets as bedgraph [Default: %(default)s]')
+  parser.add_argument('--head', dest='head_i',
+                      default=0, type=int,
+                      help='Parameters head to evaluate [Default: %(default)s]') 
+  parser.add_argument('-o', '--out_dir',
+                      default='test_out',
+                      help='Output directory for test statistics [Default: %(default)s]')
+  parser.add_argument('--rank',
+                      default=False, action='store_true',
+                      help='Compute Spearman rank correlation [Default: %(default)s]')
+  parser.add_argument('--rc',
+                      default=False, action='store_true',
+                      help='Average the fwd and rc predictions [Default: %(default)s]')
+  parser.add_argument('--save',
+                      default=False, action='store_true',
+                      help='Save targets and predictions numpy arrays [Default: %(default)s]')
+  parser.add_argument('--shifts',
+                      default='0',
+                      help='Ensemble prediction shifts [Default: %(default)s]')
+  parser.add_argument('--step',
+                      default=1, type='int',
+                      help='Step across positions [Default: %(default)s]')
+  parser.add_argument('-t', '--targets_file',
+                      default=None, type='str',
+                      help='File specifying target indexes and labels in table format')
+  parser.add_argument('--split', 
+                      default='test', choices=['train', 'valid', 'test'],
+                      help='Dataset split label for eg TFR pattern [Default: %(default)s]')
+  parser.add_argument('--tfr_pattern',
+                      default=None,
+                      help='TFR pattern string appended to data_dir/tfrecords for subsetting [Default: %(default)s]')
+  
+  parser.add_argument('params_file',
+      help='JSON file with model parameters')
+  parser.add_argument('model_file',
+      help='Trained model HDF5.')
+  parser.add_argument('data_dir',
+      help='Train/valid/test data directory')
+  args = parser.parse_args()
+
+  os.makedirs(args.out_dir, exist_ok=True)
+
+  # parse shifts to integers
+  args.shifts = [int(shift) for shift in args.shifts.split(',')]
+
+  #######################################################
+  # inputs
+
+  # read targets
+  if args.targets_file is None:
+    args.targets_file = '%s/targets.txt' % args.data_dir
+  targets_df = pd.read_csv(args.targets_file, index_col=0, sep='\t')
+
+  # read model parameters
+  with open(args.params_file) as params_open:
+    params = json.load(params_open)
+  params_model = params['model']
+  params_train = params['train']
+
+  # set strand pairs
+  if 'strand_pair' in targets_df.columns:
+    params_model['strand_pair'] = [np.array(targets_df.strand_pair)]
+
+  # construct eval data
+  eval_data = dataset.SeqDataset(args.data_dir,
+    split_label=args.split,
+    batch_size=params_train['batch_size'],
+    mode='eval',
+    tfr_pattern=args.tfr_pattern)
+
+  # initialize model
+  seqnn_model = seqnn.SeqNN(params_model)
+  seqnn_model.restore(args.model_file, args.head_i)
+  seqnn_model.build_ensemble(args.rc, args.shifts)
+
+  #######################################################
+  # evaluate
+  loss_label = params_train.get('loss', 'poisson').lower()
+  spec_weight = params_train.get('spec_weight', 1)
+  loss_fn = trainer.parse_loss(loss_label, spec_weight=spec_weight)
+  
+  # evaluate
+  test_loss, test_metric1, test_metric2 = seqnn_model.evaluate(eval_data,
+    loss_label=loss_label, loss_fn=loss_fn)
+
+  # print summary statistics
+  print('\nTest Loss:         %7.5f' % test_loss)
+
+  if loss_label == 'bce':
+    print('Test AUROC:        %7.5f' % test_metric1.mean())
+    print('Test AUPRC:        %7.5f' % test_metric2.mean())
+
+    # write target-level statistics
+    targets_acc_df = pd.DataFrame({
+      'index': targets_df.index,
+      'auroc': test_metric1,
+      'auprc': test_metric2,
+      'identifier': targets_df.identifier,
+      'description': targets_df.description
+      })
+
+  else:
+    print('Test PearsonR:     %7.5f' % test_metric1.mean())
+    print('Test R2:           %7.5f' % test_metric2.mean())
+
+    # write target-level statistics
+    targets_acc_df = pd.DataFrame({
+      'index': targets_df.index,
+      'pearsonr': test_metric1,
+      'r2': test_metric2,
+      'identifier': targets_df.identifier,
+      'description': targets_df.description
+      })
+
+  targets_acc_df.to_csv('%s/acc.txt'%args.out_dir, sep='\t',
+                        index=False, float_format='%.5f')
+  
+  #######################################################
+  # if we want to save/spearman, predict again
+
+  if args.save or args.rank:
+    # compute predictions
+    test_preds = seqnn_model.predict(eval_data, stream=True,
+                                     step=args.step, dtype='float16')
+
+    # read targets
+    test_targets = eval_data.numpy(return_inputs=False, step=args.step)
+
+    if args.rank:
+      # compute target spearmanr
+      test_spearmanr = []
+      for ti in tqdm(range(test_preds.shape[-1])):
+        test_preds_flat = test_preds[...,ti].flatten()
+        test_targets_flat = test_targets[...,ti].flatten()
+        spear_ti = spearmanr(test_targets_flat, test_preds_flat)[0]
+        test_spearmanr.append(spear_ti)
+
+      # write target-level statistics
+      targets_acc_df = pd.DataFrame({
+        'index': targets_df.index,
+        'pearsonr': test_metric1,
+        'spearmanr': test_spearmanr,
+        'r2': test_metric2,
+        'identifier': targets_df.identifier,
+        'description': targets_df.description
+        })
+
+      targets_acc_df.to_csv('%s/acc.txt'%args.out_dir, sep='\t',
+                            index=False, float_format='%.5f')
+
+  if args.save:
+    with h5py.File('%s/preds.h5' % args.out_dir, 'w') as preds_h5:
+      preds_h5.create_dataset('preds', data=test_preds)
+    with h5py.File('%s/targets.h5' % args.out_dir, 'w') as targets_h5:
+      targets_h5.create_dataset('targets', data=test_targets)
+
+    if args.bedgraph_indexes is not None:
+      bedgraph_indexes = [int(ti) for ti in args.bedgraph_indexes.split(',')]
+      bedg_out = '%s/bedgraph' % args.out_dir
+      bed.write_bedgraph(test_preds, test_targets, args.data_dir,
+        bedg_out, args.split, bedgraph_indexes)
+
+
+################################################################################
+# __main__
+################################################################################
+if __name__ == '__main__':
+  main()
