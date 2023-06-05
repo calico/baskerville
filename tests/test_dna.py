67bb2f803962c178e212099159139fa8a61d95d9
lruizcalico <lruiz@calicolabs.com>
2023-06-05 12:10:20 -0700

    refactor test_dna.py

--- tests/test_dna.py
+++ tests/test_dna.py
@@ -11,45 +11,58 @@ def test_dna_rc():
     assert seq_rc == "TGTAATC"
 
 
-def test_dna_1hot():
-    seq_dna = "ACGT"
-    seq_1hot = dna.dna_1hot(seq_dna)
-    seq_1hot_hard = np.array(
-        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype="bool"
-    )
+dna_1hot_cases = [
+    (
+        "ACGT",
+        False,
+        False,
+        np.array(
+            [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype="bool"
+        ),
+    ),
+    (
+        "ACNGT",
+        False,
+        False,
+        np.array(
+            [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
+            dtype="bool",
+        ),
+    ),
+    (
+        "ACNGT",
+        True,
+        False,
+        np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0.25] * 4, [0, 0, 1, 0], [0, 0, 0, 1]]),
+    ),
+]
+
+
+@pytest.mark.parametrize("seq_dna, n_uniform, n_sample, seq_1hot_hard", dna_1hot_cases)
+def test_dna_1hot(seq_dna, n_uniform, n_sample, seq_1hot_hard):
+    seq_1hot = dna.dna_1hot(seq_dna, n_uniform=n_uniform, n_sample=n_sample)
     assert np.array_equal(seq_1hot_hard, seq_1hot)
 
-    seq_dna = "ACNGT"
-    seq_1hot = dna.dna_1hot(seq_dna)
-    seq_1hot_hard = np.array(
-        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
-        dtype="bool",
-    )
-    assert np.array_equal(seq_1hot_hard, seq_1hot)
 
-    seq_dna = "ACNGT"
-    seq_1hot = dna.dna_1hot(seq_dna, n_uniform=True)
-    seq_1hot_hard = np.array(
-        [[1, 0, 0, 0], [0, 1, 0, 0], [0.25] * 4, [0, 0, 1, 0], [0, 0, 0, 1]]
-    )
-    assert np.array_equal(seq_1hot_hard, seq_1hot)
-
+def test_dna_1hot_sum():
     seq_dna = "ACNGT"
     seq_1hot = dna.dna_1hot(seq_dna, n_sample=True)
     assert seq_1hot[2].sum() == 1
 
 
-def test_dna_1hot_index():
-    seq_dna = "ACGT"
+dna_1hot_index_cases = [
+    ("ACGT", np.array([0, 1, 2, 3], dtype="uint8")),
+    ("ACNGT", np.array([0, 1, 4, 2, 3], dtype="uint8")),
+]
+
+
+@pytest.mark.parametrize("seq_dna, seq_1hot_hard", dna_1hot_index_cases)
+def test_dna_1hot_index(seq_dna, seq_1hot_hard):
     seq_1hot = dna.dna_1hot_index(seq_dna)
-    seq_1hot_hard = np.array([0, 1, 2, 3], dtype="uint8")
     assert np.array_equal(seq_1hot_hard, seq_1hot)
 
-    seq_dna = "ACNGT"
-    seq_1hot = dna.dna_1hot_index(seq_dna)
-    seq_1hot_hard = np.array([0, 1, 4, 2, 3], dtype="uint8")
-    assert np.array_equal(seq_1hot_hard, seq_1hot)
 
+def test_dna_1hot_index_sample():
     seq_dna = "ACNGT"
     seq_1hot = dna.dna_1hot_index(seq_dna, n_sample=True)
     assert seq_1hot[2] in [0, 1, 2, 3]
@@ -107,40 +120,22 @@ def test_hot1_rc():
     assert seq2 == seq2_rcrc
 
 
-def test_hot1_augment():
-    seq = "GATTACA"
-    seq1 = dna.dna_1hot(seq)
-    seqs1 = np.array([seq1])
+seqs1 = np.array([dna.dna_1hot("GATTACA")])
+dna_1hot_augment_cases = [
+    (seqs1, True, 0, "GATTACA"),
+    (seqs1, False, 0, "TGTAATC"),
+    (seqs1, True, 1, "NGATTAC"),
+    (seqs1, False, 1, "GTAATCN"),
+    (seqs1, True, -1, "ATTACAN"),
+    (seqs1, False, -1, "NTGTAAT"),
+]
 
-    # forward, shift 0
-    aseqs1_fwd0 = dna.hot1_augment(seqs1, True, 0)
-    aseq_fwd0 = dna.hot1_dna(aseqs1_fwd0)[0]
-    assert "GATTACA" == aseq_fwd0
 
-    # reverse, shift 0
-    aseqs1_rc0 = dna.hot1_augment(seqs1, False, 0)
-    aseq_rc0 = dna.hot1_dna(aseqs1_rc0)[0]
-    assert "TGTAATC" == aseq_rc0
-
-    # forward, shift 1
-    aseqs1_fwd1 = dna.hot1_augment(seqs1, True, 1)
-    aseq_fwd1 = dna.hot1_dna(aseqs1_fwd1)[0]
-    assert "NGATTAC" == aseq_fwd1
-
-    # reverse, shift 1
-    aseqs1_rc1 = dna.hot1_augment(seqs1, False, 1)
-    aseq_rc1 = dna.hot1_dna(aseqs1_rc1)[0]
-    assert "GTAATCN" == aseq_rc1
-
-    # forward, shift 1
-    aseqs1_fwd_m1 = dna.hot1_augment(seqs1, True, -1)
-    aseq_fwd_m1 = dna.hot1_dna(aseqs1_fwd_m1)[0]
-    assert "ATTACAN" == aseq_fwd_m1
-
-    # reverse, shift 1
-    aseqs1_rc_m1 = dna.hot1_augment(seqs1, False, -1)
-    aseq_rc_m1 = dna.hot1_dna(aseqs1_rc_m1)[0]
-    assert "NTGTAAT" == aseq_rc_m1
+@pytest.mark.parametrize("seq_dna, fwdrc, shift, seq_aug_cmp", dna_1hot_augment_cases)
+def test_dna_1hot_augment(seq_dna, fwdrc, shift, seq_aug_cmp):
+    seq_aug = dna.hot1_augment(seq_dna, fwdrc=fwdrc, shift=shift)
+    seq_aug_1hot = dna.hot1_dna(seq_aug)[0]
+    assert seq_aug_cmp == seq_aug_1hot
 
 
 def test_hot1_set():
