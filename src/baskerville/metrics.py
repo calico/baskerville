6973dac3265084e7bbb9a6f6592cdf8dd4fce9e6
lruizcalico <lruiz@calicolabs.com>
2023-08-24 12:28:56 -0700

    fix to GPU availability

--- src/baskerville/metrics.py
+++ src/baskerville/metrics.py
@@ -20,6 +20,10 @@ from tensorflow.python.keras.utils import losses_utils
 from tensorflow.python.keras.losses import LossFunctionWrapper
 from tensorflow.python.keras.utils import metrics_utils
 
+gpu_devices = tf.config.experimental.list_physical_devices("GPU")
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
+
 ################################################################################
 # Losses
 ################################################################################
